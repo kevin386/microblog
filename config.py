fc067ed5e7271bebe18139c99f41c36d3d640587
Kevin Luo <null386@gmail.com>
2016-10-26 10:06:42 +0800

    rename db repository

--- config.py
+++ config.py
@@ -15,4 +15,4 @@ OPENID_PROVIDERS = [
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_respository')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
