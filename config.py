d12461a54f7ce7299f4e9542a83197a429ebce86
Kevin Luo <null386@gmail.com>
2016-10-27 18:28:04 +0800

    页面样式

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ CSRF_ENABLED = True
 SECRET_KEY = "34c0df91-51f3-4bed-b097-60b8cc8d1963"
 
 OPENID_PROVIDERS = [
-    # {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
-    # {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
-    # {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
-    # {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
-    # {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}
     {'name': 'OpenID', 'url': 'http://pythonull.openid.org.cn/'},
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}
 ]
 
 basedir = os.path.abspath(os.path.dirname(__file__))
