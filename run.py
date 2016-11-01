a851d3bee57ab86191dc6a2c37bb095c343b34f9
Kevin Luo <null386@gmail.com>
2016-11-01 17:28:53 +0800

    作为后台程序启动时,不需要执行run

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
+# -*- coding: utf-8 -*-
 # from werkzeug.contrib.profiler import ProfilerMiddleware
 from app import app
 
 # # app.config['PROFILE'] = True
 # app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[30])
-app.run(debug=app.config['DEBUG'])
+
+if __name__ == '__main__':
+    # 作为后台程序启动时,不需要执行run
+    app.run(debug=app.config['DEBUG'])
